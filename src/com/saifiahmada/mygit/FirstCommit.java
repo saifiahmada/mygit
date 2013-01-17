b974325a0a0dad2d49d4954d292ba6c4e6bc4a29
Saifi Ahmada <saifiahmada@gmail.com>
2013-01-17 14:55:54 +0800

    first commit

---
+++ src/com/saifiahmada/mygit/FirstCommit.java
@@ -0,0 +1,12 @@
+package com.saifiahmada.mygit;
+
+/** @author Saifi Ahmada Jan 17, 2013 2:51:44 PM  **/
+
+public class FirstCommit {
+	
+	public static void main (String [] args){
+		System.out.println("This is my first commit");
+	}
+
+}
+
