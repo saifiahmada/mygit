2b2e9f63f0bf87d5f3e358699ece020b1755c4ab
Saifi Ahmada <saifiahmada@gmail.com>
2013-01-17 15:36:19 +0800

    commit lagi

---
+++ src/com/saifiahmada/mygit/TestingCommit.java
@@ -0,0 +1,11 @@
+package com.saifiahmada.mygit;
+
+/** @author Saifi Ahmada Jan 17, 2013 3:33:49 PM  **/
+
+public class TestingCommit {
+	public static void main (String [] args){
+		System.out.println("Testing commit from Eclipse Workspace");
+		System.out.println("Testing commit :: klik kanan team > push");
+	}
+}
+
